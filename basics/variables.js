b0be117313f351722642de53a77a7e58c32ff52a
Mradul Tiwari <162187272+Mysterio-17@users.noreply.github.com>
2025-01-23 19:44:10 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,5 @@
+const accountID = 1445553
+let accountEmail = "hjadhbq@google.com"
+var accountPAss = "123"
+accountCity="bengaluru"
+console.table([accountID, accountEmail, accountPAss, accountCity])
