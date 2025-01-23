a2af291df734853d0b357baf42b01ae2cb7ccd7f
Mradul Tiwari <162187272+Mysterio-17@users.noreply.github.com>
2025-01-23 19:43:33 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+let name ="mradul"
+let age =18
+let isLoggedIn= false
+
+ //number => 2 ki power 53
+ //bigint 
+ //string=> ""
+ //boolean 
+ //null =>standalone value 
+ //undefined 
+ //symbol => unique
+
+
+//object 
+
+console.log(typeof "mradul");
