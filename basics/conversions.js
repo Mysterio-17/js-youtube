860466d54086033ea271971e798167eee6a033e8
Mradul Tiwari <162187272+Mysterio-17@users.noreply.github.com>
2025-01-25 11:22:50 +0000

    changes

---
+++ basics/conversions.js
@@ -0,0 +1,6 @@
+// console.log("1+2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(2+1+"2");
+
+console.log(+true);
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -21,3 +21,24 @@ console.log(typeof score);
 let x= Number(score)
 console.log(typeof x);
 console.log(x);
+
+// primtive 
+// 7 types : string , number , boolean, null, undefined, symbol, bigInt
+// 
+// reference 
+// arrays , objects, functions
+
+// arrays
+const heros = ["ironman", "thor", "hulk"];
+
+// objects
+let valo ={
+    name : "don",
+    age : 18
+}
+console.log(valo);
+
+const myfunc= function () {
+    console.log("helloe");
+    
+}
