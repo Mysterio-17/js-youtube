70f0327319506298c6840ed3a5f55d830f4d3d17
Mradul Tiwari <162187272+Mysterio-17@users.noreply.github.com>
2025-01-25 20:55:43 +0000

    arrays

---
+++ arrays/arrays01.js
@@ -0,0 +1,26 @@
+const arr= [0,1,8,3,4,5]
+
+// console.log(arr[8]);
+// arr.push(7);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.includes(8));
+
+console.log("A ", arr);
+
+const myn1= arr.slice(1,3);
+
+console.log(myn1);
+
+ 
+console.log("B ", arr);
+
+const myn2= arr.splice(1,3);
+ console.log(myn2);
+
+ console.log("C ", arr);
+ 
+  
---
+++ arrays/arrays02.js
@@ -0,0 +1,10 @@
+const a = ["reebok" , "asics", "skechers"];
+const b = ["nike","puma", "adidas"];
+
+// console.log(a.concat(b));
+// const c = [...a, ...b]
+// console.log(c);
+
+const d= [3,4,5,[3,45,34,[54,9,7,12]]]
+
+console.log(d.flat(Infinity));
