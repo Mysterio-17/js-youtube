667a25ff5f28ddba429caa6ad1407b673dcb6411
Mradul Tiwari <162187272+Mysterio-17@users.noreply.github.com>
2025-01-26 19:42:02 +0000

    this

---
+++ arrow/this.js
@@ -0,0 +1,21 @@
+// const user ={
+//     username : "mradul",
+//     price : 99 ,
+
+//     membership : function() {
+//         console.log(`${this.username} , HI `);
+        
+//     }
+// }
+// user.membership();
+// user.username = "helo";
+// user.membership();
+
+const ADD = (a,b) => {
+    return a+b
+}
+
+const ADD2 = (a,b) => ( a + b )
+
+console.log(ADD2(5,7));
+
