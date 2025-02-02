b00d6263f4cddb744d774f597db01eff33cc4faf
Mradul Tiwari <162187272+Mysterio-17@users.noreply.github.com>
2025-02-02 20:06:52 +0000

    further

--- projects/01project.js
+++ projects/01project.js
@@ -1,3 +1,5 @@
+// first proj
+
 const button = document.querySelectorAll(".button");
 // console.log(button);
 
@@ -28,3 +30,33 @@ button.forEach(function (btn) {
 HTML AND REST CSS DOWN HERE
 https://stackblitz.com/edit/dom-project-chaiaurcode-ajqwm6y6?file=1-colorChanger%2Fchaiaurcode.js,1-colorChanger%2Findex.html 
 */
+
+// second proj
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = 'Please give a valid height';
+  }
+
+  if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = 'Please give a valid weight';
+  } else {
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+    if (BMI <= 18.6) {
+      results.innerHTML = `<span> Under Weight at ${BMI} </span>`;
+    } else if (BMI <= 24.9 && BMI > 18.6) {
+      results.innerHTML = `<span> Normal Weight at ${BMI} </span>`;
+    } else results.innerHTML = `<span> Over Weight at ${BMI} </span>`;
+  }
+});
+
+
